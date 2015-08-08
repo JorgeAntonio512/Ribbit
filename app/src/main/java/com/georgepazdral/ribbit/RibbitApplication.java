cd15ad5c5cc69f40859b49975002d86b47494af5
JorgeAntonio512 <jorge.antonio.de.segundo@gmail.com>
2015-08-07 22:03:26 -0500

    set up Parse

---
+++ app/src/main/java/com/georgepazdral/ribbit/RibbitApplication.java
@@ -0,0 +1,25 @@
+package com.georgepazdral.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by georgepazdral on 8/7/15.
+ */
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "JvgWRRsbFsuAsKFJVqmSaI2S98DCnQoIDH4tfF6I", "ugfbedjmmAThUNv4Iy1Yi2S2cSszTuR9g7IK2yPB");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
